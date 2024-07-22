332b44c155612b3067d14091b3885a901d85a1aa
NoyeArk <qilei.zhou6@gmail.com>
2024-07-22 10:33:46 +0800

    d2l：10

--- 第3章 线性神经网络/softmax回归的从零开始实现.py
+++ 第3章 线性神经网络/softmax回归的从零开始实现.py
@@ -12,6 +12,21 @@ num_inputs = 784
 num_outputs = 10
 
 
+class Accumulator:
+	"""在n个变量上累加"""
+	def __init__(self, n):
+		self.data = [0.0] * n
+	
+	def add(self, *args):
+		self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+	def reset(self):
+		self.data = [0.0] * len(self.data)
+
+	def __getitem__(self, idx):
+		return self.data[idx]
+
+
 def get_fashion_mnist_labels(labels):
 	"""构造数据集的文本标签"""
 	text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
@@ -60,9 +75,76 @@ def load_data_fashion_mnist(batch_size, resize=None):
 
 
 def softmax(X):
-	X_exp = torch.exp(X)
+	X_exp = torch.exp(X_exp)
 	partition = X_exp.sum(1, keepdim=True)
-	return X_exp / partition
+	return X_exp / partition  # 使用广播机制
+
+
+def net(X):
+	"""定义模型"""
+	return softmax(torch.matmul(X.reshape(-1, W.shape[0]), W) + b)
+
+
+def cross_entropy(y_hat, y):
+	"""定义损失函数"""
+	return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+def accuracy(y_hat, y):
+	"""计算预测正确的数量"""
+	if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+		y_hat = y_hat.argmax(axis=1)
+	cmp = y_hat.type(y.dtype) == y
+	return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):
+	"""计算在指定数据集上模型的精度"""
+	if isinstance(net, torch.nn.Module):
+		net.eval()  # 评估模式
+	metric = Accumulator(2)
+	with torch.no_grad():
+		for X, y in data_iter:
+			metric.add(accuracy(net(X), y), y.numel())
+	return metric[0] / metric[1]
+
+
+def train_epoch(net, train_iter, loss, updater):
+	"""训练模型一个迭代周期"""
+	if isinstance(net, torch.nn.Module):
+		net.train()
+	# 训练损失总和、训练准确度总和、样本数
+	metric = Accumulator(3)
+	for X, y in train_iter:
+		# 计算梯度并更新参数
+		y_hat = net(X)
+		l = loss(y_hat, y)
+		if isinstance(updater, torch.optim.Optimizer):
+			# 使用Pytorch内置的优化器和损失函数
+			updater.zero_grad()
+			l.mean().backward()
+			updater.step()
+		else:
+			# 使用定制的优化器和损失函数
+			l.sum().backward()
+			updater(X.shape[0])
+		metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+	# 返回训练损失和训练精度
+	return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train(net, train_iter, test_iter, loss, num_epochs, updater):
+	"""训练模型"""
+	animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+	for epoch in range(num_epochs):
+		train_metrics = train_epoch(net, train_iter, loss, updater)
+		test_acc = evaluate_accuracy(net, test_iter)
+		animator.add(epoch + 1, train_metrics + (test_acc,))
+	train_loss, train_acc = train_metrics
+	assert train_loss < 0.5, train_loss
+	assert train_acc <= 1 and train_acc > 0.7, train_acc
+	assert test_acc <= 1 and test_acc > 0.7, test_acc
 
 
 if __name__ == "__main__":
@@ -95,3 +177,7 @@ if __name__ == "__main__":
 	W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
 	b = torch.zeros(num_outputs, requires_grad=True)
 
+	# accuracy(y_hat, y) / len(y)
+
+	# 训练
+	
