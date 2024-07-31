4a88a5812472ff4c3dda784d0e586491cf72ef96
NoyeArk <qilei.zhou6@gmail.com>
2024-07-31 18:19:50 +0800

    kaggle：CHP

---
+++ kaggle实战1：预测房价/train.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+from model import Model
+
+
+def train(net, train_features, train_labels, test_features, test_labels, num_epochs, )
+
+
+if __name__ == "__main__":
+
+	model = Model()
+	loss = nn.MSELoss()
+	optim = nn.Adam(lr=0.001)
+
