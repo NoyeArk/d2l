ac7c58edc5b4c69ed6cd0c1009ea0a9ac951e5bc
NoyeArk <qilei.zhou6@gmail.com>
2024-09-04 09:10:46 +0800

    d2l：38

--- 第3章 线性神经网络/softmax回归的简洁实现.py
+++ 第3章 线性神经网络/softmax回归的简洁实现.py
@@ -1,68 +1,38 @@
-"""
-线性回归的简洁实现
-1. 生成数据
-2. 构建模型
-3. 选择损失函数
-4. 选择优化器
-5. 进行训练
-    5.1 取一个批次的数据
-    5.2 送入模型进行预测
-    5.3 计算损失
-    5.4 梯度清零
-    5.5 计算梯度
-    5.6 反向传播
-"""
 import torch
 import torch.nn as nn
-from torch.utils import data
+import d2l.torch as d2l
 
 
-def generate_data(w, b, num_examples):
-    X = torch.normal(0, 1, (num_examples, len(w)))
-    y = torch.matmul(X, w) + b
-    y += torch.normal(0, 0.001, y.shape)
-    return X, y.reshape(-1, 1)
+def accuracy(y_hat, y):
+    y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
 
 
 if __name__ == '__main__':
-    # 训练超参数
-    lr = 0.03
-    epochs = 3
-    batch_size = 12
+    # 超参数
+    lr = 0.05
+    epochs = 10
+    batch_size = 256
 
-    true_w = torch.tensor([2, -3.4])
-    true_b = torch.tensor(4.2)
+    # 加载数据集
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-    # 构造数据集
-    features, labels = generate_data(true_w, true_b, 1000)
-
-    # 读取数据集
-    dataset = data.TensorDataset(*(features, labels))
-    data_iter = data.DataLoader(dataset, batch_size, shuffle=True)
-
-    # 定义模型
-    net = nn.Sequential(nn.Linear(2, 1))
-
-    # 定义损失函数
-    loss = nn.MSELoss()
-
-    # 定义优化算法
+    net = nn.Sequential(nn.Linear(784, 10))
+    loss = nn.CrossEntropyLoss(reduction='none')
     optim = torch.optim.SGD(net.parameters(), lr)
 
-    # 训练模型
-    for epoch in range(epochs):
-        for X, y in data_iter:
+    for epochs in range(epochs):
+        net.train()
+        for X, y in train_iter:
             y_hat = net(X)
             _loss = loss(y_hat, y)
             optim.zero_grad()
             _loss.backward()
             optim.step()
 
-        _loss = loss(net(features), labels)
-        print(f'epoch {epoch + 1}, loss {_loss:f}')
+        net.eval()
+        with torch.no_grad():
+            for X, y in test_iter:
+                pass
 
-    # 查看训练结果
-    w = net[0].weight.data
-    print('w的估计误差：', true_w - w.reshape(true_w.shape))
-    b = net[0].bias.data
-    print('b的估计误差：', true_b - b)
--- 第3章 线性神经网络/线性回归的简洁实现.py
+++ 第3章 线性神经网络/线性回归的简洁实现.py
@@ -1,62 +1,69 @@
+"""
+线性回归的简洁实现
+1. 生成数据
+2. 构建模型
+3. 选择损失函数
+4. 选择优化器
+5. 进行训练
+    5.1 取一个批次的数据
+    5.2 送入模型进行预测
+    5.3 计算损失
+    5.4 梯度清零
+    5.5 计算梯度
+    5.6 反向传播
+"""
+
 import torch
-from torch import nn
-from torch.utils import data # type: ignore
+import torch.nn as nn
+from torch.utils import data
 
 
-def synthetic_data(w, b, num_examples):
-	"""生成数据集"""
-	X = torch.normal(0, 1, (num_examples, len(w)))
-	y = torch.matmul(X, w) + b
-	y += torch.normal(0, 0.001, y.shape)
-	return X, y.reshape(-1, 1)
+def generate_data(w, b, num_examples):
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.001, y.shape)
+    return X, y.reshape(-1, 1)
 
 
-epoches = 3
-batch_size = 10
+if __name__ == '__main__':
+    # 训练超参数
+    lr = 0.03
+    epochs = 3
+    batch_size = 12
 
+    true_w = torch.tensor([2, -3.4])
+    true_b = torch.tensor(4.2)
 
-if __name__ == "__main__":
-	true_w = torch.tensor([2, -3.4])
-	true_b = 4.2
-	
-	# 构造数据集
-	features, labels = synthetic_data(true_w, true_b, 1000)
+    # 构造数据集
+    features, labels = generate_data(true_w, true_b, 1000)
 
-	# 读取数据集
-	dataset = data.TensorDataset(*(features, labels))
-	data_iter = data.DataLoader(dataset, batch_size, shuffle=True)
+    # 读取数据集
+    dataset = data.TensorDataset(*(features, labels))
+    data_iter = data.DataLoader(dataset, batch_size, shuffle=True)
 
-	# 定义模型
-	net = nn.Sequential(nn.Linear(2, 1))
+    # 定义模型
+    net = nn.Sequential(nn.Linear(2, 1))
 
-	# 初始化模型参数（可以没有）
-	net[0].weight.data.normal_(0, 0.01)
-	net[0].bias.data.fill_(0)
+    # 定义损失函数
+    loss = nn.MSELoss()
 
-	# 定义损失函数
-	loss = nn.HuberLoss(reduction='sum', delta=0.1)
-	# loss = nn.MSELoss()
+    # 定义优化算法
+    optim = torch.optim.SGD(net.parameters(), lr)
 
-	# 定义优化算法
-	trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+    # 训练模型
+    for epoch in range(epochs):
+        for X, y in data_iter:
+            y_hat = net(X)
+            _loss = loss(y_hat, y)
+            optim.zero_grad()
+            _loss.backward()
+            optim.step()
 
-	# 训练神经网络
-	for epoch in range(epoches):
-		for X, y in data_iter:
-			y_pred = net(X)
-			l = loss(y_pred, y)
-			# 已经计算完梯度了 所以后面不需要记录梯度
-			trainer.zero_grad()
-			# 反向传播计算梯度
-			l.backward()
-			# 更新模型参数
-			trainer.step()
-			
-		l = loss(net(features), labels)
-		print(f'epoch {epoch + 1}, loss {l:f}')
-	
-	# 查看训练结果
-	w = net[0].weight.data
-	print('w的估计误差：', true_w - w.reshape(true_w.shape))
-	b = net[0].bias.data
-	print('b的估计误差：', true_b - b)
+        _loss = loss(net(features), labels)
+        print(f'epoch {epoch + 1}, loss {_loss:f}')
+
+    # 查看训练结果
+    w = net[0].weight.data
+    print('w的估计误差：', true_w - w.reshape(true_w.shape))
+    b = net[0].bias.data
+    print('b的估计误差：', true_b - b)
