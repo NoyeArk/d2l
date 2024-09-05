6fc0a5b350e4f107d18fa7f8118a96561354be64
NoyeArk <qilei.zhou6@gmail.com>
2024-09-05 09:16:11 +0800

    d2l review：chapter3

--- 第3章 线性神经网络/softmax回归的简洁实现.py
+++ 第3章 线性神经网络/softmax回归的简洁实现.py
@@ -11,28 +11,35 @@ def accuracy(y_hat, y):
 
 if __name__ == '__main__':
     # 超参数
-    lr = 0.05
+    lr = 0.005
     epochs = 10
     batch_size = 256
 
     # 加载数据集
     train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-    net = nn.Sequential(nn.Linear(784, 10))
+    net = nn.Sequential(nn.Flatten(), nn.Linear(784, 10))
     loss = nn.CrossEntropyLoss(reduction='none')
-    optim = torch.optim.SGD(net.parameters(), lr)
+    optim = torch.optim.Adam(net.parameters(), lr)
 
-    for epochs in range(epochs):
+    for epoch in range(epochs):
+        metric = d2l.Accumulator(3)
         net.train()
         for X, y in train_iter:
             y_hat = net(X)
             _loss = loss(y_hat, y)
             optim.zero_grad()
-            _loss.backward()
+            _loss.mean().backward()
             optim.step()
+        metric.add(float(_loss.sum()), accuracy(y_hat, y), y.numel())
 
+        print(f'epoch:{epoch}, loss:{metric[0] / metric[2]}, acc:{metric[1] / metric[2]}')
+
+        # 计算在训练集上的模型的精度
+        metric = d2l.Accumulator(2)
         net.eval()
         with torch.no_grad():
             for X, y in test_iter:
-                pass
+                metric.add(accuracy(net(X), y), y.numel())
 
+            print(f'epoch:{epoch}, acc:{metric[0] / metric[1]}')
